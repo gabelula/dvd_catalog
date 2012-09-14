0c03652d2370bad0da9f10e571895e33eee0e543
Gabriela Rodriguez <gabelula@gmail.com>
2012-09-14 13:59:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DvdCatalog::Application.config.secret_token = 'f63faaca62ec24473e473d4416c1a24c5f19e194608a13b2aabc6558f953407129769538a589e5c24ca7c28ff3046da9d14b868b9ab9fec7f40a4e5b4b460533'
