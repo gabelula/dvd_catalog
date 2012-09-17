5eca4cfa32e64c691d5b8ef6cfa1ddc01baa7129
Gabriela Rodriguez <gabelula@gmail.com>
2012-09-17 10:40:20 -0700

    jquery rails

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module DvdCatalog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_view.javascript_expansions[:default] = %w( jquery rails )
   end
 end
