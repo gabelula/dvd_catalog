2b16beaec7b66e7138b85dad6716446bf36efa4f
Gabriela Rodriguez <gabelula@gmail.com>
2012-09-17 10:42:29 -0700

    Add specs.

---
+++ spec/models/director_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Director do
+  describe 'Relationships' do
+    before do
+      @disc = Factory(:disc)
+    #  @director = Factory(:director, @disc, :discs => [@disc])
+    end
+
+    it 'should add discs' do
+
+    end
+
+    it 'should remove discs' do
+
+    end
+
+  end
+end
