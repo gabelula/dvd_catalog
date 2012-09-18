2f43d45d3db873bb7c84924853f056bec3c753ea
Gabriela Rodriguez <gabelula@gmail.com>
2012-09-18 15:19:54 -0700

    Remove some files added by mistake.

--- spec/factories/actors.rb
+++
@@ -1,5 +0,0 @@
-Factory.define :actor do |p|
-  p.add_attribute :name, 'Juana Perez'
-  p.add_attribute :birth_date, Date.new(1977, 3, 2)
-  p.add_attribute :gender, 'F'
-end
--- spec/factories/directors.rb
+++
@@ -1,5 +0,0 @@
-Factory.define :director do |dir|
-  dir.name       'Maria Rodriguez'
-  dir.birth_date Date.new(1977, 3, 2)
-  dir.gender     'F'
-end
