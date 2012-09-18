d10dfdd932d9a9305eaaa20e2dd78fffc64feab1
Gabriela Rodriguez <gabelula@gmail.com>
2012-09-18 09:39:27 -0700

    It was duplicating the factories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'rspec/rails'
 require 'rspec/autorun'
 
 require 'factory_girl_rails'
-FactoryGirl.find_definitions
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
